5f519fc56544badf72efc52421a7b92d44951494
Henning-1025 <sthe082025@gmail.com>
2025-11-05 13:25:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ class Program
 {
     static void Main(string[] args)
     {
+      Console.Write("Input Password: ");
+      string password = Console.ReadLine();
       
+      Console.WriteLine("Password length: " + password.Length);
+
+      if (password.Length < 8)
+        {
+            Console.WriteLine("Password is too short -- it should be atleast 8 characters");
+        }
+      else
+        {
+            Console.WriteLine("Password is okay!");
+        }
     }
 }
